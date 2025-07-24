9e6ec6ec2c067082f6bb6fb289740950dc32a87e
Maureen E <maureenekeugo1@gmail.com>
2025-07-23 22:32:40 -0400

    Implemented login endpoint with JWT

--- server/src/routes/authRoutes.ts
+++ server/src/routes/authRoutes.ts
@@ -1,11 +1,13 @@
 import { Router, Request,Response } from "express";
 import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export const authRouter = Router();
 
 const prisma = new PrismaClient();
 
+//Register new users 
 authRouter.post("/", async (req:Request, res:Response)=>{
 
 const {name, email, password} = req.body;
@@ -19,7 +21,7 @@ try {
     //check if user already exist 
     const existUser = await prisma.user.findUnique({where: {email}});
     if(existUser){
-        res.status(400).json({message: "User already exists"})
+       return res.status(400).json({message: "User already exists"})
     }
 
     //hash password 
@@ -46,4 +48,46 @@ try {
     
 }
 
+})
+
+//Login user 
+authRouter.post("/login", async (req:Request, res:Response)=>{
+    const {email, password} = req.body;
+
+    try {
+        //check if email exist in the db
+        const user = await prisma.user.findUnique({where : {email}})
+        if(!user){
+            return res.status(400).json({message: "Invalid credentials"})
+        };
+
+        //check if password matches hashed password
+        const comparePassword = await bcrypt.compare(password,user.password)
+        if(!comparePassword){
+            return res.status(400).json({message: "Invalid password"})
+        }
+
+        //generate JWT
+        const token = jwt.sign(
+            {id:user.id,email:user.email},
+            process.env.JWT_SECRET as string,
+            {expiresIn:"1hr"}
+
+        )
+
+        //remove password form User model
+        const {password:_,...returningUser} = user;
+
+        //inform the client side that new user was successfully created and sent new user data without the password 
+        res.status(200).json({
+            message:"User is successfully logged-in ",
+            token: token,
+            user: returningUser 
+            });
+        
+
+    } catch (error) {
+        console.error("Login error:",error);
+        res.status(500).json({message: "Login error"})
+    }
 })
